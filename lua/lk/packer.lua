daf8625b9d1564211275a8908630c515bc647dc6
Liang Kui <kui.liang@shopee.com>
2022-12-18 13:45:44 +0800

    file tree

--- lua/lk/packer.lua
+++ lua/lk/packer.lua
@@ -46,4 +46,12 @@ return require('packer').startup(function(use)
 			{ 'rafamadriz/friendly-snippets' },
 		}
 	}
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
--- lua/lk/remap.lua
+++ lua/lk/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>nl", vim.cmd.nohlsearch)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
