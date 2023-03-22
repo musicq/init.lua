b8eec3005c7509c00bf5bd5965cb709061e204b2
Liang Kui <kui.liang@shopee.com>
2023-03-22 17:48:03 +0800

    update

--- lua/lk/remap.lua
+++ lua/lk/remap.lua
@@ -1,8 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>h", vim.cmd.nohlsearch)
-
+vim.keymap.set("n", "<leader>nl", vim.cmd.nohlsearch)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -17,7 +16,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
