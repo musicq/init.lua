98875d51e9d80b1f9ba68e53aa65332911808b69
Liang Kui <kui.liang.sg@gmail.com>
2024-04-11 09:14:21 +0800

    remap oil

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -8,10 +8,27 @@ require('oil').setup({
   },
   view_options = {
     show_hidden = true,
-    is_hidden_file = function(name, bufnr)
-      return vim.startswith(name, './') or vim.startswith(name, '../')
-    end,
-  }
+    -- is_always_hidden = function(name, _) return name == '..' end,
+    natural_order = true
+  },
+  keymaps = {
+    ["g?"] = "actions.show_help",
+    ["l"] = "actions.select",
+    ["<C-s>"] = "actions.select_vsplit",
+    ["<C-h>"] = "actions.select_split",
+    ["<C-t>"] = "actions.select_tab",
+    ["<C-p>"] = "actions.preview",
+    ["<C-c>"] = "actions.close",
+    ["<C-l>"] = "actions.refresh",
+    ["h"] = "actions.parent",
+    ["_"] = "actions.open_cwd",
+    ["`"] = "actions.cd",
+    ["~"] = "actions.tcd",
+    ["gs"] = "actions.change_sort",
+    ["gx"] = "actions.open_external",
+    ["g."] = "actions.toggle_hidden",
+    ["g\\"] = "actions.toggle_trash",
+  },
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
