6a2ee98388b9172dc566304307c0e160c94ad4c8
Liang Kui <kui.liang.sg@gmail.com>
2024-04-16 11:48:58 +0800

    add default lsp

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,5 +1,23 @@
 require('mason').setup({})
 require('mason-lspconfig').setup({
+  ensure_installed = {
+    "clangd",
+    "cspell",
+    "cssls",
+    "css-variables-language-server",
+    "cssmodules_ls",
+    "eslint_d",
+    "gopls",
+    "jq",
+    "lua_ls",
+    "prettier",
+    "prettierd",
+    "rust_analyzer",
+    "tailwindcss",
+    "tsserver",
+    "yamlls"
+  },
+
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
